72959308c27bf914d00b108469691b0b1cca3d7b
doucet <homedoucetpaul@gmail.com>
2022-04-11 20:57:23 -0400

    implements working beautifulsoup scraper

--- dockerAWS/test.py
+++ dockerAWS/test.py
@@ -10,7 +10,7 @@ import urllib
 from sqlalchemy import create_engine
 from selenium_stealth import stealth
 import pymysql
-import undetected_chromedriver as uc
+#import undetected_chromedriver as uc
 from sqlalchemy.engine.url import URL
 from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
@@ -23,38 +23,36 @@ def handler(event=None, context=None):
 
     URL_SEARCH = "https://www.tickpick.com/concerts/yeat-tickets/"
 
-    #options = webdriver.ChromeOptions()
-    options = uc.ChromeOptions()
+    options = webdriver.ChromeOptions()
+    #options = uc.ChromeOptions()
     options.binary_location = '/opt/chrome/chrome'
     #options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('start-maximized')
-    options.add_argument('enable-automation')
-    options.add_argument('--disable-infobars')
+    options.add_argument("--disable-setuid-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
     options.add_argument("--single-process")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-dev-tools")
-    options.add_argument('--disable-browser-side-navigation')
     options.add_argument("--no-zygote")
     options.add_argument(f"--user-data-dir={mkdtemp()}")
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
 
-    #driver = webdriver.Chrome("/opt/chromedriver", options=options)
-    driver = uc.Chrome(options=options)
+    driver = webdriver.Chrome("/opt/chromedriver", options=options)
+    #driver = uc.Chrome(options=options)
 
     print("driver started")
 
-    # stealth(driver,
-    #     languages=["en-US", "en"],
-    #     vendor="Google Inc.",
-    #     platform="Win32",
-    #     webgl_vendor="Intel Inc.",
-    #     renderer="Intel Iris OpenGL Engine",
-    #     fix_hairline=True,
-    #     )
+    stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 
     driver.get(URL_SEARCH)
 
@@ -73,9 +71,14 @@ def handler(event=None, context=None):
     venue_names = list(map(lambda elem: elem.text, venue_names))
     dates = list(map(lambda elem: elem.text, dates))
 
+    return
+
     print(links)
 
     def add_page_to_row_list(link, row_list, driver, venue_name, date, time):
+        if(link == 'https://www.tickpick.com/buy-yeat-tickets-newport-music-hall-4-9-22-7pm/5076846/' or 
+            link == "https://www.tickpick.com/buy-lyrical-lemonade-summer-smash-post-malone-playboi-carti-young-thug-gunna-3-day-pass-tickets-douglass-park-6-17-22-3am/5101091/"):
+            return
         driver.get(link)
 
         xpath = "//*[@id=\"listingContainer\"]/div[*]"
@@ -85,23 +88,25 @@ def handler(event=None, context=None):
         while len(elements) == 0:
             elements = driver.find_elements_by_xpath(xpath)
 
-        print(element[0].get_attribute('innerHTML'))
 
+        for elem in elements:
+            dic = {}
+            dic["Date"] = time
+            dic["VenueID"] = venue_name
+            dic["DateEvent"] = date
+            dic["Quantity"] = elem.find_element_by_tag_name("select").text.split()[0]
+            
+            
+            #print("getting the price")
+            #print(elem.get_attribute("innerHTML"))
+            price = ""
+            while len(price) == 0:
+                #price = elem.find_element(by=By.XPATH, value="//label/b").get_attribute("textContent")
+                price = elem.find_element(by=By.TAG_NAME, value="b").get_attribute("textContent")
 
-    for elem in elements:
-        dic = {}
-        dic["Date"] = time
-        dic["VenueID"] = venue_name
-        dic["DateEvent"] = date
-        dic["Quantity"] = elem.find_element_by_tag_name("select").text.split()[0]
-        
-        print(venue_name)
-        
-        price = ""
-        while len(price) == 0:
-            price = elem.find_element(by=By.TAG_NAME, value="b").text
-        dic["Price"] = price[1:]
-        row_list.append(dic)
+            #print("finish getting price")
+            dic["Price"] = price[1:]
+            row_list.append(dic)
 
     row_list = []
 
